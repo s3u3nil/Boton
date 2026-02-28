77d0a04a47f710a14c2d47cd3e691b8fe6486310
s3u3nil <29424.sunil@gmail.com>
2026-02-28 23:58:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@
 const express = require('express');
 const fs      = require('fs');
 const path    = require('path');
-
+require('./bot');
 const app = express();
 app.use(express.json());
 app.use(express.static(__dirname));
